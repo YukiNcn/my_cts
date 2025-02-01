2f9b4a87665b00a7b0ae29a449519fc10712e067
YukiNcn <yuankaiyou345@gmail.com>
2025-02-01 12:56:39 +0800

    Remove debug print

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,4 @@
 # 2025-01-20 17:29:17 - Add input validation
 # 2025-01-22 17:02:58 - Add input validation
 # 2025-01-29 20:07:23 - Fix off-by-one error
+# 2025-02-01 12:56:39 - Add input validation
