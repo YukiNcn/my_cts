0950b4db69016585544ed92012700dc162328f58
YukiNcn <yuankaiyou345@gmail.com>
2025-02-25 23:44:39 +0800

    Refactor helper function

--- src/main.py
+++ src/main.py
@@ -11,3 +11,4 @@ print('Hello')
 # 2025-02-18 14:15:40 - Refactor helper function
 # 2025-02-19 20:51:54 - Update function signature
 # 2025-02-23 20:21:32 - Add input validation
+# 2025-02-25 23:44:39 - Add error handling
