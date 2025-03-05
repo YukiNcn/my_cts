e4f2e6b5181fdc32e802a0aa1550236ebf1dd449
YukiNcn <yuankaiyou345@gmail.com>
2025-03-05 20:36:47 +0800

    Update function signature

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,4 @@
 # 2025-02-01 12:56:39 - Add input validation
 # 2025-03-03 20:20:36 - Optimize loop performance
 # 2025-03-03 20:30:42 - Fix off-by-one error
+# 2025-03-05 20:36:47 - Update function signature
