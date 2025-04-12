f684dfff04b8c55eebe20e35c2389eed9a91b29d
YukiNcn <yuankaiyou345@gmail.com>
2025-04-12 19:38:38 +0800

    Refactor helper function

--- src/main.py
+++ src/main.py
@@ -24,3 +24,4 @@ print('Hello')
 # 2025-04-06 14:31:42 - Refactor helper function
 # 2025-04-08 23:48:08 - Fix off-by-one error
 # 2025-04-12 19:33:05 - Remove debug print
+# 2025-04-12 19:38:38 - Add input validation
