3287242913dc216ddadac2f14834f7c5e4710fa5
YukiNcn <yuankaiyou345@gmail.com>
2025-04-24 20:52:47 +0800

    Fix off-by-one error

--- src/main.py
+++ src/main.py
@@ -27,3 +27,4 @@ print('Hello')
 # 2025-04-12 19:38:38 - Add input validation
 # 2025-04-16 16:32:39 - Add error handling
 # 2025-04-24 20:30:35 - Refactor helper function
+# 2025-04-24 20:52:47 - Fix off-by-one error
