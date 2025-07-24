a9a9234aa6467c89c242bd297533811fd704fe43
YukiNcn <yuankaiyou345@gmail.com>
2025-07-24 23:03:05 +0800

    Add input validation

--- src/main.py
+++ src/main.py
@@ -47,3 +47,4 @@ print('Hello')
 # 2025-07-14 13:04:17 - Refactor helper function
 # 2025-07-16 21:42:04 - Update function signature
 # 2025-07-24 22:53:17 - Remove debug print
+# 2025-07-24 23:03:05 - Remove debug print
