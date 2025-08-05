accaf66a3f09b3fe0e61da04eb23f6f0e4111f20
YukiNcn <yuankaiyou345@gmail.com>
2025-08-05 19:47:29 +0800

    Fix off-by-one error

--- src/main.py
+++ src/main.py
@@ -54,3 +54,4 @@ print('Hello')
 # 2025-07-30 12:55:17 - Optimize loop performance
 # 2025-07-30 17:31:16 - Optimize loop performance
 # 2025-08-01 16:41:17 - Update function signature
+# 2025-08-05 19:47:29 - Add error handling
