413f292a0d8a715a4ee00f21267f34aa206010c3
YukiNcn <yuankaiyou345@gmail.com>
2025-08-18 16:28:58 +0800

    Fix off-by-one error

--- src/utils.py
+++ src/utils.py
@@ -35,3 +35,4 @@
 # 2025-08-04 17:32:18 - Optimize loop performance
 # 2025-08-08 17:49:28 - Add input validation
 # 2025-08-15 10:29:52 - Refactor helper function
+# 2025-08-18 16:28:58 - Update function signature
