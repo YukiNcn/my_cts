8eb9dd4686c3af503ffdcf1d0476ccac379d6561
YukiNcn <yuankaiyou345@gmail.com>
2025-08-20 18:01:29 +0800

    Remove debug print

--- src/utils.py
+++ src/utils.py
@@ -37,3 +37,4 @@
 # 2025-08-15 10:29:52 - Refactor helper function
 # 2025-08-18 16:28:58 - Update function signature
 # 2025-08-19 16:20:54 - Optimize loop performance
+# 2025-08-20 18:01:29 - Optimize loop performance
