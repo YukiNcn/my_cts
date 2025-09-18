cfef338ade15cfe4ff9794e9778c49be5e24d0a1
YukiNcn <yuankaiyou345@gmail.com>
2025-09-18 11:41:42 +0800

    Optimize loop performance

--- src/utils.py
+++ src/utils.py
@@ -41,3 +41,4 @@
 # 2025-08-26 10:13:11 - Update function signature
 # 2025-09-01 19:32:26 - Optimize loop performance
 # 2025-09-10 21:16:49 - Optimize loop performance
+# 2025-09-18 11:41:42 - Add error handling
