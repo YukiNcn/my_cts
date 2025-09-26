78bb9a2bfb8a1cd01a1a2e442d3b9ba5b2dd07ff
YukiNcn <yuankaiyou345@gmail.com>
2025-09-26 15:56:40 +0800

    Add error handling

--- src/main.py
+++ src/main.py
@@ -64,3 +64,4 @@ print('Hello')
 # 2025-09-17 12:43:52 - Remove debug print
 # 2025-09-23 23:09:59 - Fix off-by-one error
 # 2025-09-24 19:22:39 - Optimize loop performance
+# 2025-09-26 15:56:40 - Fix off-by-one error
