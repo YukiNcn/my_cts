359d8a37ed560085c7ea8d5f4d0ec30cfc65de6d
YukiNcn <yuankaiyou345@gmail.com>
2025-10-13 20:18:51 +0800

    Update function signature

--- src/utils.py
+++ src/utils.py
@@ -43,3 +43,4 @@
 # 2025-09-10 21:16:49 - Optimize loop performance
 # 2025-09-18 11:41:42 - Add error handling
 # 2025-10-10 15:18:02 - Optimize loop performance
+# 2025-10-13 20:18:51 - Fix off-by-one error
