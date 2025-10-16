54f00602b2f8df7a8afead4fc9828d408803122a
YukiNcn <yuankaiyou345@gmail.com>
2025-10-16 20:20:00 +0800

    Refactor helper function

--- src/utils.py
+++ src/utils.py
@@ -48,3 +48,4 @@
 # 2025-10-14 17:20:03 - Optimize loop performance
 # 2025-10-15 22:59:43 - Update function signature
 # 2025-10-15 23:11:14 - Refactor helper function
+# 2025-10-16 20:20:00 - Refactor helper function
