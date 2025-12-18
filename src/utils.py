ec1c0d6080cd752f98de329b2b8ad9eef96b9f97
YukiNcn <yuankaiyou345@gmail.com>
2025-12-18 11:41:16 +0800

    Add input validation

--- src/utils.py
+++ src/utils.py
@@ -62,3 +62,4 @@
 # 2025-12-13 10:04:08 - Remove debug print
 # 2025-12-15 12:37:02 - Update function signature
 # 2025-12-18 11:31:49 - Add input validation
+# 2025-12-18 11:41:16 - Add error handling
